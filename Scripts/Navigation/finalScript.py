452e705bd851ad9add4ed7117739f2338cadadc2
Andres Rello <arello@innomius.com>
2023-07-07 14:36:27 -0400

    chore: real board distances

--- Scripts/Navigation/finalScript.py
+++ Scripts/Navigation/finalScript.py
@@ -5,114 +5,114 @@ cyclesInfo = [
     {
         "name": "cycle 0",
         "mainNavDirection": 270,
-        "endOfCycleDistance": 0.20,
+        "endOfCycleDistance": 0.60,
         "finalPoint": [
             {
                 # horizontal navigation
                 "name": "start point horizontal navigation",
                 "lidarDirection": 270,
-                "targetDistance": 0.15
+                "targetDistance": 0.3
             },
             {
                 # vertical navigation
                 "name": "start point vertical navigation",
-                "lidarDirection": 180,
-                "targetDistance": 0.40
+                "lidarDirection": 0,
+                "targetDistance": 0.93
             }
         ]
     },
     {
         "name": "cycle 1",
         "mainNavDirection": 90,
-        "endOfCycleDistance": 0.20,
+        "endOfCycleDistance": 0.60,
         "finalPoint": [
             {
                 # horizontal navigation
                 "name": "start point horizontal navigation",
                 "lidarDirection": 90,
-                "targetDistance": 0.15
+                "targetDistance": 0.30
             },
             {
                 # vertical navigation
                 "name": "start point vertical navigation",
                 "lidarDirection": 0,
-                "targetDistance": 0.40
+                "targetDistance": 0.93
             },
         ],
     },
     {
         "name": "cycle 2",
         "mainNavDirection": 270,
-        "endOfCycleDistance": 0.20,
+        "endOfCycleDistance": 0.6,
         "finalPoint": [
             {
                 # horizontal navigation
                 "name": "start point horizontal navigation",
                 "lidarDirection": 270,
-                "targetDistance": 0.15
+                "targetDistance": 0.3
             },
             {
                 # vertical navigation
                 "name": "start point vertical navigation",
                 "lidarDirection": 180,
-                "targetDistance": 0.40
+                "targetDistance": 0.89
             },
         ],
     },
     {
         "name": "cycle 3",
         "mainNavDirection": 90,
-        "endOfCycleDistance": 0.20,
+        "endOfCycleDistance": 0.6,
         "finalPoint": [
             {
                 # horizontal navigation
                 "name": "start point horizontal navigation",
                 "lidarDirection": 90,
-                "targetDistance": 0.15
+                "targetDistance": 0.8
             },
             {
                 # vertical navigation
                 "name": "start point vertical navigation",
                 "lidarDirection": 180,
-                "targetDistance": 0.40
+                "targetDistance": 0.89
             },
         ],
     },
     {
         "name": "cycle 4",
         "mainNavDirection": 270,
-        "endOfCycleDistance": 0.20,
+        "endOfCycleDistance": 0.6,
         "finalPoint": [
             {
                 # horizontal navigation
                 "name": "start point horizontal navigation",
                 "lidarDirection": 270,
-                "targetDistance": 0.15
+                "targetDistance": 0.30
             },
             {
                 # vertical navigation
                 "name": "start point vertical navigation",
                 "lidarDirection": 180,
-                "targetDistance": 0.15
+                "targetDistance": 0.30
             },
         ],
     },
     {
         "name": "cycle 5",
         "mainNavDirection": 90,
-        "endOfCycleDistance": 0.20,
+        "endOfCycleDistance": 0.6,
         "finalPoint": [
             {
                 # horizontal navigation
                 "name": "start point horizontal navigation",
                 "lidarDirection": 90,
-                "targetDistance": 0.15
+                "targetDistance": 0.3
             },
             {
                 # vertical navigation
                 "name": "start point vertical navigation",
-                "lidarDirection": 180,
-                "targetDistance": 0.15
+                "lidarDirection": 0,
+                "targetDistance": 0.3
             },
         ],
     },  
